a24529d60f843b038d98f6b33111e34fbeedd2bb
MELLI2022 <denisenkoswetlana109@gmail.com>
2022-09-01 13:40:41 +0300

    Проверьте дз 4

--- src/Main.java
+++ src/Main.java
@@ -2,116 +2,127 @@ import javax.swing.plaf.synth.SynthOptionPaneUI;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Домашнее задание 4");
 
-        // Задание 1
-        int age1 = 19;
-        if (age1 >= 18) {
-            System.out.println("Поздравляем с совершенолетием");
-        }
-        int age2 = 17;
-        if (age2 < 18) {
-            System.out.println("Возраст совершенолетия ещё не наступил");
-        }
-
-        int childAge = 8;
-        if (childAge >= 7) {
-            System.out.println("Ребенок может посещать школу");
-        }
-
-        int human1 = 20;
-        if (human1 > 18) {
-            System.out.println("Человек может учиться в университете");
-        }
-
-        int human2 = 25;
-        if (human2 >= 24) {
-            System.out.println("Человеку пора искать первую работу");
-        }
-
-        int wagonOccypancy = 102;
-        int seatPlace = 60;
-        int standingPlace = wagonOccypancy - seatPlace;
-        System.out.println("Колличество сидячих мест в вагоне равно " + standingPlace + "");
-        int people = 103;
-        if (people > 102) {
-            System.out.println("Вагон уже полностью забит");
-        }
-
-        //Задание2
-
-        int age = 17;
-        if (age >= 18) {
-            System.out.println("Поздравить пользователя с днем рождения");
-        } else {
-            System.out.println("Возраст совершеннолетия ещё не наступил, и нужно немного подождать");
-        }
-
-        int human = 6;
-        if (human >= 7) {
-            System.out.println("Может ходить в школу");
-        } else {
-            System.out.println("Возраст для школы ещё не наступил, нужно ходить в детский сад");
-        }
-
-        int youtn = 19;
-        if (youtn >= 18) {
-            System.out.println("Может ходить в универ");
-        } else {
-            System.out.println("Возраст для универа ещё не наступил, нужно ходить в школу");
-        }
-
-        int youngWomen = 25;
-        if (youngWomen >= 24) {
-            System.out.println("Пора искать первую работу");
-        } else {
-            System.out.println("Возраст для поиска первой работы ещё не наступил, можно учиться в университете");
-        }
-
-        int allPlace = 103;
-        if (allPlace <= 102) {
-            System.out.println("В вагоне есть свободные места");
-        } else {
-            System.out.println("Вагон уже полностью забит");
-        }
-
-        // Задание 3
-        int child = 4;
-        if (child >= 2 && child <= 6) {
-            System.out.println("Если возраст человека равен 4 , то ему нужно ходить в детский сад ");
-            int human3 = 17;
-            if (human3 >= 7 && human3 <= 18) {
-                System.out.println("Если возраст человека равен 17, то ему нужно ходить в школу");
+        {// Задание 1.1
+            int age = 24;
+            if (age >= 18) {
+                System.out.println("Поздравляем с совершенолетием");
             }
-            int human4 = 22;
-            if (human4 >= 18 && human4 < 24) {
-                System.out.println("Если возраст человека равен 22, то ему нужно ходить в университет");
+            if (age < 18) {
+                System.out.println("Возраст совершенолетия ещё не наступил");
+            }
+            //Задание 1.2
+            if (age >= 7 && age < 18) {
+                System.out.println("Ребенок может посещать школу");
+            }
+            if (age >= 18 && age < 24) {
+                System.out.println("Человек может учиться в университете");
+            }
+            if (age >= 24) {
+                System.out.println("Человеку пора искать первую работу");
+            }
+            //Задание1.3
+
+            int wagonOccypancy = 102;
+            int seatPlace = 60;
+            int standingPlace = wagonOccypancy - seatPlace;
+            System.out.println("Колличество сидячих мест в вагоне равно " + standingPlace + "");
+            int seat = 59;
+            int stand = 41;
+            if (seat == seatPlace) {
+                System.out.println("Сидячих мест нет");
+            }
+            if (stand == standingPlace) {
+                System.out.println("Стоячих мест нет");
+            }
+            if (stand < standingPlace) {
+                System.out.println("В вагоне есть " + (standingPlace - stand) + " стоячee мест");
+            }
+            if (seat < seatPlace) {
+                System.out.println("В вагоне есть " + (seatPlace - seat) + " сидячee мест");
+            }
+
+        }
+        {// Задание 2.1
+            int age = 24;
+            if (age >= 18) {
+                System.out.println("Поздравляем с совершенолетием");
+            } else if (age < 18) {
+                System.out.println("Возраст совершенолетия ещё не наступил");
+            }
+            //Задание 2.2
+            if (age >= 7 && age < 18) {
+                System.out.println("Ребенок может посещать школу");
+            } else if (age >= 18 && age < 24) {
+                System.out.println("Человек может учиться в университете");
+            } else if (age >= 24) {
+                System.out.println("Человеку пора искать первую работу");
+            }
+            //Задание2.3
+
+            int wagonOccypancy = 102;
+            int seatPlace = 60;
+            int standingPlace = wagonOccypancy - seatPlace;
+            System.out.println("Колличество сидячих мест в вагоне равно " + standingPlace + "");
+            int seat = 61;
+            int stand = 42;
+            if (seat < seatPlace) {
+                System.out.println("В вагоне есть " + (seatPlace - seat) + " сидячee мест");
             } else {
-                System.out.println("Он должен ходить на работу");
+                System.out.println("Сидячих мест нет");
+            }
+            if (stand < standingPlace) {
+                System.out.println("В вагоне есть " + (standingPlace - stand) + " стоячee мест");
+            } else {
+                System.out.println("Стоячих мест нет");
             }
 
-            int child1 = 3;
-            if (child1 < 5) {
-                System.out.println("Ребенку возраст которого равен 3 нельзя кататься на атракционе");
-            }
-
-            int child2 = 12;
-            if (child2 > 5 && child2 < 14) {
-                System.out.println("Ребенок может кататься только в сопровождении взрослого,если взрослого нет, то кататься нельзя");
-            }
-            else {
-                System.out.println("Ребенок может кататься без сопровождении взрослого");
-            }
-            int one = 1;
-            int two = 2;
-            int free = 3;
-            if (two > two)
-            { System.out.println(two);}
-            else if (two<free){
-                    System.out.println(free);}
         }
-    }
-}
+        {// Задание 3.1
+            int personAge = 15;
+            if (personAge >= 2 && personAge <= 6) {
+                System.out.println("Если возраст человека равен " + personAge + " то ему нужно ходить в детский садик");
+            } else if (personAge >= 7 && personAge <= 18) {
+                System.out.println("Если возраст человека равен " + personAge + " то ему нужно ходить в школу");
+            } else if (personAge > 18 && personAge < 24) {
+                System.out.println("Если возраст человека равен " + personAge + " то ему нужно ходить в университет");
+            } else {
+                System.out.println("Если возраст человека равен" + personAge + " то ему нужно идти на работу");
+            }
+            // Задание 3.2
+            if (personAge < 5) {
+                System.out.println("Если возраст человека равен " + personAge + " то он не может кататься на атракционе");
+            } else if (personAge >= 5 && personAge < 14) {
+                System.out.println("Если возраст человека равен " + personAge + " то он может кататься на атракционе в соопровождении взрослых");
+            } else {
+                System.out.println("Если возраст человека равен " + personAge + " то он может кататься на атракционе самостоятельно");
+            }
+            // Задание 3.2
+            int one = 4;
+            int two = 56;
+            int three = 60;
+            if (one > two) {
+                    System.out.println("Большее число " + one + "");
+                } else if (two > one) {
+                        System.out.println("Большее число " + two + "");
+                    } else {
+                        System.out.println("Большее число " + three + "");
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
